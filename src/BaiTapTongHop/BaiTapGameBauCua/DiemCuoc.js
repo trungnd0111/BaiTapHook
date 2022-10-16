a590784ddc415a584b160c87d7f1ce51453389ae
trungnd0111 <114087486+trungnd0111@users.noreply.github.com>
2022-10-16 12:13:06 +0700

    Update DiemCuoc.js

--- src/BaiTapTongHop/BaiTapGameBauCua/DiemCuoc.js
+++ src/BaiTapTongHop/BaiTapGameBauCua/DiemCuoc.js
@@ -9,7 +9,7 @@ export default function DiemCuoc(props) {
 
     return (
         <div>
-            <h3 className="text-center display-4 " style={{color:'#58FA58',marginTop:0}}>GAME BẦU CUA CYBERLEARN </h3>
+            <h3 className="text-center display-4 " style={{color:'#58FA58',marginTop:0}}>GAME BẦU CUA</h3>
 
             <div className="text-center mt-5">
                 <span style={{fontSize:'20px',borderRadius:'5%'}} className="p-3 text-white bg-danger">Tiền thưởng: <span className="text-warning">{tongDiem.toLocaleString()}$</span></span>
